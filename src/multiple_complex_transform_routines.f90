a6d822e2f8939fbee936262a8345001fc0c762a4
jlokimlin <jlokimlin@gmail.com>
2016-07-12 14:16:20 -0700

    1D complex backward and forward are enclosed in a submodule

---
+++ src/multiple_complex_transform_routines.f90
@@ -0,0 +1,6 @@
+!cfftmi multiple complex initialization
+!cfftmb multiple complex backward
+!cfftmf multiple complex forward
+!submodule (fftpack_routines) multiple_complex_transform_routines
+!
+!end submodule multiple_complex_transform_routines
---
+++ src/multiple_quarter_sine_transform_routines.f90
@@ -0,0 +1,9 @@
+!sinqmi multiple real quarter-sine initialization
+!sinqmb multiple real quarter-sine backward
+!sinqmf multiple real quarter-sine forward
+!submodule (fftpack_routines) multiple_quarter_sine_transform_routines
+!
+!contains
+!
+!end submodule multiple_quarter_sine_transform_routines
+
